a0066517b77d56009b693a3f8b6c568ffe1625e9
lzx8035 <lzx8035@gmail.com>
2024-07-29 20:59:45 -0700

    Implementing the Countries List and Explore

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,38 @@
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  // const countries = cities.reduce((arr, city) => {
+  //   if (!arr.map((el) => el.country).includes(city.country))
+  //     return [...arr, { country: city.country, emoji: city.emoji }];
+  //   else return arr;
+  // }, []);
+
+  const countries = Array.from(
+    //SAME
+    new Set(
+      cities.map((city) =>
+        JSON.stringify({ country: city.country, emoji: city.emoji })
+      )
+    )
+  ).map((str) => JSON.parse(str));
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
